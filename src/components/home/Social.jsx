a78e582cd633c772b02b923fb3a840415e74195d
linlin <denglinlin315@gmail.com>
2022-12-03 00:01:24 +1000

    fix

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -12,7 +12,7 @@ const Social = () => {
             <i class="uil uil-dribbble"></i>
         </a>
 
-        <a href="https//github.com/" className="home__social-icon" target="_blank">
+        <a href="https://github.com/" className="home__social-icon" target="_blank" rel="noreferrer">
             <i class="uil uil-github-alt"></i>
         </a>
        </div>
