23f1228d64e656594c22bc55412a64b606d0e703
linlin <denglinlin315@gmail.com>
2022-12-02 23:54:02 +1000

    update2

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import "./home.css";
 
 const Social = () => {
     return (
        <div className="home__social">
-        <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+        <a href="https://www.instagram.com/" className="home__social-icon" target="_blank" rel="noreferrer">
             <i class="uil uil-instagram"></i>
         </a>
 
-        <a href="https://dribbble.com/" className="home__social-icon" target="_blank">
+        <a href="https://dribbble.com/" className="home__social-icon" target="_blank" rel="noreferrer">
             <i class="uil uil-dribbble"></i>
         </a>
 
